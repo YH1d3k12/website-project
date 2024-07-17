5c4b8ed1bc6094fc24bcf1271e405155f57aefb3
djalma <djalmahy@hotmail.com>
2024-07-17 14:35:52 -0300

    Feat: Created Golden Title Animation

--- src/features/body/components/Navbar.jsx
+++ src/features/body/components/Navbar.jsx
@@ -1,17 +1,19 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-import user from '../../../assets/user.png';
-import './navbar.css';
+import logo from "../../../assets/logo.png";
+import user from "../../../assets/user.png";
+import "./navbar.css";
 
 
 export default function Navbar() {
     return (
         <nav className="navbar">
             <div className="navbar-background">
-                <Link to={"/"} className="navbar-title">Capivara</Link>
+                <Link to={"/"} className="navbar-title"><img className="navbar-title-logo" src={logo} alt="logo capivara" />Capivara</Link>
                 <ul className="navbar-links">
                     <li><Link to={"/games"}>Games</Link></li>
                     <li><Link to={"/about"}>About</Link></li>
+                    <li><Link to={"/outro"}>Other</Link></li>
                 </ul>
                 <img className="navbar-user" src={user} alt="user" />
             </div>
