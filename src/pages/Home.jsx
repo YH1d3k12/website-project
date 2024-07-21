1770d8c1846223d63b5e33e0bc83371a24029462
djalma <djalmahy@hotmail.com>
2024-07-21 18:13:29 -0300

    Refactor: Home Section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,20 @@
 import Slider from '../features/home/components/Slider';
+import GoldFall from '../components/GoldFall';
 import '../features/home/home.css';
 
 
 export default function Home() {
     return (
-        <div className="home">
-            <Slider />
+        <div className="home section">
+            <div className="home-goldfall">
+                <GoldFall />
+            </div>
+            <div className="home-section">
+                <Slider />
+            </div>
+            <div className="home-goldfall">
+                <GoldFall />
+            </div>
         </div>
     );
 };
