4c80971b9fec40a29a2ad4cf8885e17d7a76724b
Djalma <djalmahy@hotmail.com>
2024-08-02 20:25:30 -0300

    Refactor: CSS properties

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,7 @@ export default function Home() {
             <div className="home-goldfall">
                 <GoldFall />
             </div>
-            <div className="home-section">
+            <div className="home-section scroll-y">
                 <Slider />
                 <ScoreBoard />
             </div>
